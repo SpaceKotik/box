4214af931aaacd7dcb710ff8183c91d1f4d34167
nikita <nikitaoskolkov@inbox.ru>
2020-02-09 00:21:09 +0300

    vot tak

--- list.c
+++ list.c
@@ -1,30 +1,76 @@
 #include "list.h"
 #include <malloc.h>
-#include <time.h>
 #include <stdlib.h>
 
-struct _list{
+#define _POISON 0
+
+typedef struct _elem_t{
+	data_t data;
+	struct _elem_t* next;
+	struct _elem_t* prev;
+} elem_t;
+
+typedef struct _list_t{
 	box b;
-	char* name;
-};
+	elem_t* head;
+	elem_t* tail;
+	int size;
+}list_t;
 
-typedef struct _list list;
+//comment
+void list_push_back(box *list_b, data_t data_new) {
+	list_t *list = (list_t*)(list_b);
+	elem_t *elem = (elem_t*)calloc(1, sizeof(elem_t));
+	elem->data = data_new;
+	if (list->size == 0){
+		list->head = elem;
+		list->tail = elem;
+		elem->next = NULL;
+		elem->prev = NULL;
+	} else {
+		elem->next = NULL;
+		elem->prev = list->tail;
+		list->tail = elem;
+	}
+}
 
+void list_pop_back(box *list_b) {
+	list_t *list = (list_t*)(list_b);
+	printf("%d\n", list->tail->data);
+	list->tail->data = _POISON;
+	if (list->size == 1) {
+		free(list->tail);
+		list->tail = NULL;
+		list->head = NULL;
+	} else {
+		list->tail->prev = NULL;
+		list->tail = list->tail->prev;
+		free(list->tail->next);
+		list->tail->next = NULL;
+	}
+}
 
+int list_size(box *list_b){
+	list_t *list = (list_t*)(list_b);
+	return list->size;
+}
 
-/*box* list_create() {
-	box *temp = (box*)calloc(1, sizeof(box));
-	return temp;
-}*/
+void list_resize(box *list_b){
+	list_t *list = (list_t*)(list_b);
 
-void list_Dtor(box* list) {
-	//free(list);
+}
+
+void list_Dtor(box* list_b) {
+	list_t *list = (list_t*)(list_b);
+	//free(elem);
 }
 
 box* list_Ctor() {
-	printf("a\n");
-	list* list = (struct _list*)(calloc(1, sizeof(list)));
-	list->name = "list_n";
+	printf("List_Ctor\n");
+	list_t* list = (list_t*)calloc(1, sizeof(list_t));
+	list->head = NULL;
+	list->tail = NULL;
+	list->size = 0;
 	list->b.Dtor = list_Dtor;
 	return (box*) list;
 }
