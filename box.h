2820518c754c9fa812ca2a106abff21cd3856f4e
nikita <nikitaoskolkov@inbox.ru>
2020-02-09 00:21:57 +0300

    push and pop in list

--- box.h
+++ box.h
@@ -1,17 +1,17 @@
 #pragma once
+
 struct _box{
-	//print/destory/insert
 	void (*Dtor)(struct _box *box);
 	void (*print_all_elems)(struct _box *box);
-	//void (*push_back)(struct _box *box, int elem);
-	//void (*pop_back)(struct _box *box);
+	void (*push_back)(struct _box *box, int elem);
+	void (*pop_back)(struct _box *box);
 	void (*insert)(struct _box *box, int pos, int elem);
 	int (*erase)(struct _box *box, int pos);
 	int (*capacity)(struct _box *box);
 	int (*size)(struct _box *box);
 	//int (*max_size)(struct _box *box);
 	//void (*shrink_to_fit)(struct _box *box);
-	//void (*resize)(struct _box *box);
+	void (*resize)(struct _box *box);
 	void (*clear)(struct _box *box);
 
 };
--- list.h
+++ list.h
@@ -3,4 +3,11 @@
 
 //публичный интерфейс к листу
 
+typedef int data_t;
+
+void list_push_back(struct _box *box, data_t data);
+void list_pop_back(struct _box *box);
+int list_size(struct _box *box);
+void list_resize(struct _box *box);
+
 box* list_Ctor();
