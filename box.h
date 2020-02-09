c343cac05f44aac05190142ab345736c93b56001
Spacekotik <speedcuber17@yandex.ru>
2020-02-09 17:18:29 +0300

    lots of segfaults

--- box.h
+++ box.h
@@ -3,7 +3,7 @@ struct _box{
 	//print/destory/insert
 	void (*Dtor)(struct _box *box);
 	void (*print_all_elems)(struct _box *box);
-	//void (*push_back)(struct _box *box, int elem);
+	void (*push_back)(struct _box *box, int elem);
 	//void (*pop_back)(struct _box *box);
 	void (*insert)(struct _box *box, int pos, int elem);
 	int (*erase)(struct _box *box, int pos);
@@ -13,7 +13,6 @@ struct _box{
 	//void (*shrink_to_fit)(struct _box *box);
 	//void (*resize)(struct _box *box);
 	void (*clear)(struct _box *box);
-
 };
 
 typedef struct _box box;
